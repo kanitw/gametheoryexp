7fa93cdf762a1011680a71fc7efe0af26c00cfc3
ccwei <ccwei@stanford.edu>
2012-02-03 21:15:10 -0800

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ GameTheoryExp::Application.routes.draw do
   get   '/login', :to => 'sessions#new', :as => :login
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/auth/failure', :to => 'sessions#failure'
+  get '/logout', :to => 'sessions#destroy'
 
 
   # Sample of named route:
