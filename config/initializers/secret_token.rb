4d688ff20a86b729757ad64c67260eaac81f01b5
ccwei <ccwei@stanford.edu>
2012-02-03 19:54:49 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameTheoryExp::Application.config.secret_token = '19c9166978552f716daeb3cb0bcbff004ee1c0d7408c4b8c4d7936293d9983cb0afae1fcb32f7e976a7ff0eaef6bf1bb131083651cd65431f38b51d31900b054'
